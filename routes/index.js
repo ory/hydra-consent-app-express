74efa1b2ee8503788ac3c6bddd64973ddf40bdfe
Aeneas <arekkas@users.noreply.github.com>
2017-10-05 23:43:09 +0200

    implement new consent flow (#2)

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,42 @@
 const express = require('express')
-const Hydra = require('hydra-js')
-const path = require('path')
+const Hydra = require('ory-hydra-sdk')
 const router = express.Router()
+const OAuth2 = require('simple-oauth2')
+
+const scope = 'hydra.consent hydra.consent.*'
+const oauth2 = OAuth2.create({
+  client: {
+    id: process.env.HYDRA_CLIENT_ID,
+    secret: process.env.HYDRA_CLIENT_SECRET
+  },
+  auth: {
+    tokenHost: endpoint = process.env.HYDRA_URL,
+    authorizePath: authorizePath = '/oauth2/auth',
+    tokenPath: tokenPath = '/oauth2/token'
+  },
+  options: {
+    useBodyAuth: false,
+    useBasicAuthorizationHeader: true
+  }
+})
 
 // Instantiating a hydra config. If you provide no config, the hydra-js library will try to figure them out
 // from the environment variables, such as HYDRA_CLIENT_ID, HYDRA_CLIENT_SECRET, and HYDRA_URL.
-const hydra = new Hydra()
+
+Hydra.ApiClient.instance.basePath = process.env.HYDRA_URL
+
+const hydra = new Hydra.OAuth2Api()
+
+const refreshToken = () => oauth2.clientCredentials
+  .getToken({ scope })
+  .then((result) => {
+    const token = oauth2.accessToken.create(result);
+    const hydraClient = Hydra.ApiClient.instance
+    hydraClient.authentications.oauth2.accessToken = token.token.access_token
+    return Promise.resolve(token)
+  })
+
+refreshToken().then()
 
 // A simple error helper
 const catcher = (w) => (error) => {
@@ -14,9 +45,6 @@ const catcher = (w) => (error) => {
   w.status(500)
   return Promise.reject(error)
 }
-
-// Let's specify a standard redirect url where the user will be sent to if something goes wrong.
-const redir = process.env.DEFAULT_REDIRECT_URL
 
 // This is a mock object for the user. Usually, you would fetch this from, for example, mysql, or mongodb, or somewhere else.
 // The data is arbitrary, but will require a unique user id.
@@ -30,37 +58,55 @@ const user = {
   nickname: 'Danny',
 }
 
+const resolver = (resolve, reject) => (error, data, response) => {
+  if (error) {
+    return reject(error)
+  } else if (response.statusCode < 200 || response.statusCode >= 400) {
+    return reject(new Error('Consent endpoint gave status code ' + response.statusCode + ', but status code 200 was expected.'))
+  }
+
+  resolve(data)
+}
+
 // This get's executed when we want to tell hydra that the user is authenticated and that he authorized the application
-const resolveConsent = (r, w, challenge, scopes = []) => {
+const resolveConsent = (r, w, consent, grantScopes = []) => {
   const { email, email_verified, user_id: subject, name, nickname } = user
-  const data = {}
+  const idTokenExtra = {}
 
   // Sometimes the body parser doesn't return an array, so let's fix that.
-  if (!Array.isArray(scopes)) {
-    scopes = [scopes]
+  if (!Array.isArray(grantScopes)) {
+    grantScopes = [grantScopes]
   }
 
   // This is the openid 'profile' scope which should include some user profile data. (optional)
-  if (scopes.indexOf('profile') >= 0) {
-    data.name = name
-    data.nickname = nickname
+  if (grantScopes.indexOf('profile') >= 0) {
+    idTokenExtra.name = name
+    idTokenExtra.nickname = nickname
   }
 
   // This is to fulfill the openid 'email' scope which returns the user's email address. (optional)
-  if (scopes.indexOf('email') >= 0) {
-    data.email = email
-    data.email_verified = email_verified
+  if (grantScopes.indexOf('email') >= 0) {
+    idTokenExtra.email = email
+    idTokenExtra.email_verified = email_verified
   }
 
-  // Make sure that the consent challenge is valid
-  hydra.verifyConsentChallenge(challenge).then(({ challenge: decoded }) => {
-    // Create the consent response
-    return hydra.generateConsentResponse(r.query.challenge, subject, scopes, {}, data).then(({ consent }) => {
-
-      // Redirect back to hydra
-      w.redirect(decoded.redir + '&consent=' + consent)
-    })
-  }).catch(catcher(w))
+  refreshToken().then(() => {
+    // Do not return this directly, otherwise `then()` will be called, causing superagent to fail with the double
+    // callback bug.
+    hydra.getOAuth2ConsentRequest(r.query.consent,
+      resolver(
+        (consentRequest) => hydra.acceptOAuth2ConsentRequest(r.query.consent, {
+            subject,
+            grantScopes,
+            idTokenExtra,
+            accessTokenExtra: {}
+          },
+          resolver(() => w.redirect(consentRequest.redirectUrl), catcher(w))
+        ),
+        catcher(w)
+      )
+    )
+  })
 }
 
 router.get('/consent', (r, w) => {
@@ -68,50 +114,53 @@ router.get('/consent', (r, w) => {
 
   if (!r.session.isAuthenticated) {
     // The user is not authenticated yet, so redirect him to the log in page
-    return w.redirect('/login?error=Please+log+in&challenge=' + r.query.challenge)
+    return w.redirect('/login?error=Please+log+in&consent=' + r.query.consent)
   } else if (r.query.error) {
     // An error occurred (at hydra)
     return w.render('error', { error: { name: r.query.error, message: r.query.error_description } })
   }
 
-  // Ok, the user is authenticated! Let's show the consent screen!
-  hydra.verifyConsentChallenge(r.query.challenge).then(({ challenge }) => {
-    // challenge contains informations such as requested scopes, client id, ...
+  refreshToken().then(() => {
+    // Do not return this directly, otherwise `then()` will be called, causing superagent to fail with the double
+    // callback bug.
+    hydra.getOAuth2ConsentRequest(r.query.consent, resolver((consentRequest) => {
+      // consentRequest contains informations such as requested scopes, client id, ...
 
-    // Here you could, for example, allow clients to force a user's consent. Since you're able to
-    // say which scopes a client can request in hydra, you could allow this for a few highly priviledged clients!
-    //
-    // if (challenge.scp.find((s) => s === 'force-consent')) {
-    //   resolveConsent(r, w, r.query.challenge, challenge.scp)
-    //   return Promise.resolve()
-    // }
+      // Here you could, for example, allow clients to force a user's consent. Since you're able to
+      // say which scopes a client can request in hydra, you could allow this for a few highly priviledged clients!
+      //
+      // if (consentRequest.scp.find((s) => s === 'force-consent')) {
+      //   resolveConsent(r, w, r.query.consent, consentRequest.scp)
+      //   return Promise.resolve()
+      // }
 
-    // render the consent screen
-    w.render('consent', { scopes: challenge.scp })
-    return Promise.resolve()
-  }).catch(catcher(w))
+      // render the consent screen
+      w.render('consent', { scopes: consentRequest.requestedScopes })
+
+    }, catcher(w)))
+  })
 })
 
 router.post('/consent', (r, w) => {
   if (!r.session.isAuthenticated) {
-    return w.redirect('/login?error=Please+log+in&challenge=' + r.body.challenge)
+    return w.redirect('/login?error=Please+log+in&consent=' + r.body.consent)
   }
 
-  resolveConsent(r, w, r.query.challenge, r.body.allowed_scopes)
+  resolveConsent(r, w, r.query.consent, r.body.allowed_scopes)
 })
 
 router.get('/login', (r, w) => {
-  w.render('login', { error: r.query.error, user, challenge: r.query.challenge })
+  w.render('login', { error: r.query.error, user, consent: r.query.consent })
 })
 
 router.post('/login', (r, w) => {
   const form = r.body
   if (form.email !== user.email || form.password !== user.password) {
-    w.redirect('/login?error=Wrong+credentials+provided&challenge=' + form.challenge)
+    w.redirect('/login?error=Wrong+credentials+provided&consent=' + form.consent)
   }
 
   r.session.isAuthenticated = true
-  w.redirect('/consent?challenge=' + r.body.challenge)
+  w.redirect('/consent?consent=' + r.body.consent)
 })
 
 router.get('/', (r, w) => w.send('This is an exemplary consent app for Hydra. Please read the hydra docs on how to use this router.'))
